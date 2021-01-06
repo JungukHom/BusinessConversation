7f54c84549b51352e7eb2a971e30664edb0a5ea6
JungukHom <developer0223@gmail.com>
2021-01-06 17:26:52 +0900

    Add | Zone UI asset

---
+++ Assets/Scripts/UI/SceneControl/_03_Study_Youtube.cs
@@ -0,0 +1,36 @@
+﻿namespace BusinessConversation.CHN.Hotel
+{
+    // Unity
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class _03_Study_Youtube : MonoBehaviour
+    {
+        public Button btn_next;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Screen.NotifySceneLoaded();
+
+            SetListeners();
+        }
+        
+        private void SetListeners()
+        {
+            btn_next.onClick.AddListener(() => { OnNextButtonClicked(); });
+        }
+
+        private void OnNextButtonClicked()
+        {
+            Screen.FadeOut(() =>
+            {
+                SceneLoader.LoadScene(SceneName._04_Study_Read);
+            });
+        }
+    }
+}
